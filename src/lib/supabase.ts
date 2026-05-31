b4144610f1535b06fbbdd4674025da36d14b4617
joelhuzuliakdev <joelhuzuliakdev@outlook.com>
2026-05-31 18:14:59 -0300

    first commit

---
+++ src/lib/supabase.ts
@@ -0,0 +1,17 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = import.meta.env.PUBLIC_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.PUBLIC_SUPABASE_ANON_KEY
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+
+export type Product = {
+    id: number
+    name: string
+    description: string
+    price: number
+    category: string
+    available: boolean
+    image_url?: string
+    created_at?: string
+}
