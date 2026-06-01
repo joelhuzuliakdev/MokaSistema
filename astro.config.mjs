2b745058ec339cecfb2b28e4e059d638f0b0bd96
joelhuzuliakdev <joelhuzuliakdev@outlook.com>
2026-06-01 17:39:33 -0300

    elimine node

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,11 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
+import vercel from '@astrojs/vercel';
 
-import node from '@astrojs/node';
-
-// https://astro.build/config
 export default defineConfig({
   output: 'server',
+  adapter: vercel(),
   vite: {
     plugins: [tailwindcss()]
-  },
-
-  adapter: node({
-    mode: 'standalone'
-  })
+  }
 });
